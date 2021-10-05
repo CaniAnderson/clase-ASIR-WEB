0f0e0ccb63858811c0f8859bd62fc77e26e0790c
Gonzalo <gonzalo.ma36210@cesurformacion.com>
2021-10-05 10:27:57 +0200

    commit Ejercicio horario

---
+++ horario.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Horario ASIR</title>
+</head>
+<body>
+    <h1>Horario ASIR 2021/2022</h1>
+    <br>
+<?php
+echo "Fecha: ".date("d/n/Y")."<br>";
+echo "Hora: ".date("H:i:s");
+
+?>
+</body>
+</html>
